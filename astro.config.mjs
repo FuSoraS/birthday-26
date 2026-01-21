066e91a003d67468c3f1a822ae9c792eee9d5cf9
FuSoraS <developer.FuSoraS@gmail.com>
2026-01-21 15:04:37 -0300

    feat: add Minecraft font files and update styles for font usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,8 @@ export default defineConfig({
 
   env: {
     schema: {
-        NAME: envField.string({ context: 'client', access: 'public' })
+        NAME: envField.string({ context: 'client', access: 'public' }),
+        PARAGRAPHMINE: envField.string({ context: 'client', access: 'public', default: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis asperiores, commodi explicabo aspernatur laboriosam dolorem corporis doloremque numquam nesciunt cum aliquid ipsum maxime iste autem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis asperiores, commodi explicabo aspernatur laboriosam dolorem corporis doloremque numquam nesciunt cum aliquid ipsum maxime iste autem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.'})
     }
   }
 });
