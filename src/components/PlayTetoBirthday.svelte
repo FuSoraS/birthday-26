dce5c37c00d497e9a10e5b092d56f1fa78c7054f
FuSoraS <developer.FuSoraS@gmail.com>
2026-01-24 12:01:14 -0300

    style: update button layout to include icon and adjust padding

--- src/components/PlayTetoBirthday.svelte
+++ src/components/PlayTetoBirthday.svelte
@@ -15,7 +15,8 @@
 </script>
 
 <div class="z-2">
-    <button on:click={handleClick} class="bg-red-300 active:bg-red-400 text-white font-bold py-2 px-4 rounded-full">
+    <button on:click={handleClick} class="flex gap-1 bg-red-300 active:bg-red-400 text-white font-bold py-2 px-3 rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" /></svg>
         Play audio
     </button>
     <audio bind:this={audioRef}></audio>
