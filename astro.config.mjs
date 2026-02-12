b29287edbfd6c5a41b239bdad0bfe9bf38cca104
FuSoraS <developer.FuSoraS@gmail.com>
2026-02-12 14:21:05 -0300

    feat: enhance presentation and music pages with additional content variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,11 +17,14 @@ export default defineConfig({
     schema: {
         NAME: envField.string({ context: 'client', access: 'public', default: 'Nombre' }),
         URL: envField.string({ context: 'client', access: 'public', default: '' }),
+        PRESENTATION_P_1: envField.string({ context: 'client', access: 'public', default: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }),
+        PRESENTATION_P_2: envField.string({ context: 'client', access: 'public', default: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }),
         MINE_P_1: envField.string({ context: 'client', access: 'public', default: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis asperiores, commodi explicabo aspernatur laboriosam dolorem corporis doloremque numquam nesciunt cum aliquid ipsum maxime iste autem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis asperiores, commodi explicabo aspernatur laboriosam dolorem corporis doloremque numquam nesciunt cum aliquid ipsum maxime iste autem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}),
         LETTER_HEADER_1: envField.string({ context: 'client', access: 'public', default: 'Un día soleado de primavera' }),
         LETTER_HEADER_2: envField.string({ context: 'client', access: 'public', default: 'Querida compañero' }),
         MUSIC_TITLE: envField.string({ context: 'client', access: 'public', default: 'Titulo' }),
-        MUSIC_P_1: envField.string({ context: 'client', access: 'public', default: 'Parrafo 1'})
+        MUSIC_P_1: envField.string({ context: 'client', access: 'public', default: 'Parrafo 1'}),
+        MUSIC_P_2: envField.string({ context: 'client', access: 'public', default: 'Parrafo 2'})
     }
   }
 });
