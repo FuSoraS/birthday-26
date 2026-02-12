faa46ba2dc105311c9191d73857a69b12f1a7811
FuSoraS <developer.FuSoraS@gmail.com>
2026-02-12 11:51:14 -0300

    feat: update ROUTES to use BASE_URL for dynamic path generation

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
+export const BASE_URL = import.meta.env.BASE_URL;
+
 export const ROUTES = {
-  MUSIC: "/music",
-  LETTER: "/letter",
-  CRAFT: "/craft",
-  PRESENTATION: "/presentation",
+  MUSIC: `${BASE_URL}/music`,
+  LETTER: `${BASE_URL}/letter`,
+  CRAFT: `${BASE_URL}/craft`,
+  PRESENTATION: `${BASE_URL}/presentation`,
 } as const;
 
 export type ColorHex = `#${string}`;
