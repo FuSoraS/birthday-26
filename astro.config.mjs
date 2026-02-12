89d29de633f6445f7a08ac4a8897564f8091b6a0
FuSoraS <developer.FuSoraS@gmail.com>
2026-02-12 11:20:21 -0300

    feat: configure GitHub Pages deployment and set site details in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://FuSoraS.github.io',
+  base: '/birthday-26',
   integrations: [svelte()],
 
   vite: {
