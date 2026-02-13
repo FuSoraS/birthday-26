2975efa8a4ead8ac2c1c002b567a27866774d4a7
FuSoraS <developer.FuSoraS@gmail.com>
2026-02-13 16:21:44 -0300

    fix: update site URL to use correct casing for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://FuSoraS.github.io',
+  site: 'https://fusoras.github.io',
   base: '/birthday-26',
   integrations: [svelte()],
 
